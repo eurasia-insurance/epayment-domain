5ab9334efaa9e803a38fe2376877734dcba30ea8
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-10 17:13:50 +0600

    KKBMerchantSign digest is XMLValue

--- jaxb/src/main/java/com/lapsa/kkb/core/xml/KKBMerchantSign.java
+++ jaxb/src/main/java/com/lapsa/kkb/core/xml/KKBMerchantSign.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlInlineBinaryData;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlValue;
 
@@ -20,7 +19,6 @@ public class KKBMerchantSign implements Serializable {
 
     // подпись
     @XmlValue
-    @XmlInlineBinaryData
     private byte[] digest;
 
     // GENERATED
