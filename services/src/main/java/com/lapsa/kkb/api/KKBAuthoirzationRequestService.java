bfae2bdb3d43de1b701c087a5c3f6fc62762078c
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-12 23:39:38 +0600

    format

--- services/src/main/java/com/lapsa/kkb/api/KKBAuthoirzationRequestService.java
+++ services/src/main/java/com/lapsa/kkb/api/KKBAuthoirzationRequestService.java
@@ -5,5 +5,4 @@ import com.lapsa.fin.FinCurrency;
 public interface KKBAuthoirzationRequestService {
     String encodeRequest(String orderId, double amount, FinCurrency currency) throws KKBEncodingException;
     String encodeRequest(KKBAuthorizationRequest request) throws KKBEncodingException;
-    
 }
