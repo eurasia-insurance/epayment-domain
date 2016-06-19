6416ee0f8961173582c5f9d99ff0e25f365dfdf4
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-19 15:36:49 +0600

    package fix after rename

--- services/src/main/java/com/lapsa/kkb/services/KKBDocumentComposerService.java
+++ services/src/main/java/com/lapsa/kkb/services/KKBDocumentComposerService.java
@@ -1,8 +1,11 @@
 package com.lapsa.kkb.services;
 
+import com.lapsa.kkb.core.KKBCartDocument;
 import com.lapsa.kkb.core.KKBOrder;
 import com.lapsa.kkb.core.KKBPaymentRequestDocument;
 
-public interface KKBComposerService {
+public interface KKBDocumentComposerService {
     KKBPaymentRequestDocument composeRequest(KKBOrder order) throws KKBServiceError;
+
+    KKBCartDocument composeCart(KKBOrder order) throws KKBServiceError;
 }
