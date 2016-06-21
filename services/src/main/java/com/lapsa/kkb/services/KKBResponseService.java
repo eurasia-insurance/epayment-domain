56589e14eb9e807d4c4e627c1d602299696df9de
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-21 19:56:44 +0600

    API change

--- services/src/main/java/com/lapsa/kkb/services/KKBResponseService.java
+++ services/src/main/java/com/lapsa/kkb/services/KKBResponseService.java
@@ -3,11 +3,16 @@ package com.lapsa.kkb.services;
 import com.lapsa.kkb.core.KKBPaymentRequestDocument;
 import com.lapsa.kkb.core.KKBPaymentResponseDocument;
 
-public interface KKBResponseParserService {
+public interface KKBResponseService {
     String parseOrderId(String response) throws KKBServiceError, KKBFormatException;
+
+    String parseOrderId(KKBPaymentResponseDocument response) throws KKBServiceError, KKBFormatException;
 
     void validateSignature(String response) throws KKBServiceError, KKBFormatException, KKBWrongSignature;
 
-    void validateOrderResponse(KKBPaymentRequestDocument request, KKBPaymentResponseDocument response)
+    void validateSignature(KKBPaymentResponseDocument response)
+	    throws KKBServiceError, KKBFormatException, KKBWrongSignature;
+
+    void validateResponse(KKBPaymentRequestDocument request, KKBPaymentResponseDocument response)
 	    throws KKBFormatException, KKBValidationErrorException;
 }
