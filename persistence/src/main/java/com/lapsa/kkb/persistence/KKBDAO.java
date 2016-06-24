8674412b6ceb1e1b0d02ee04ff6c9193074a0469
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-24 15:30:38 +0600

    API change add byPassCache find by id

--- persistence/src/main/java/com/lapsa/kkb/persistence/KKBDAO.java
+++ persistence/src/main/java/com/lapsa/kkb/persistence/KKBDAO.java
@@ -3,7 +3,7 @@ package com.lapsa.kkb.persistence;
 public interface KKBDAO<T, I> {
     T findById(I id) throws KKBEntityNotFound, KKBPeristenceOperationFailed;
 
-    T findByIdForced(final I id) throws KKBEntityNotFound, KKBPeristenceOperationFailed;
+    T findByIdByPassCache(final I id) throws KKBEntityNotFound, KKBPeristenceOperationFailed;
 
     <Z extends T> Z save(Z entity) throws KKBPeristenceOperationFailed;
 }
