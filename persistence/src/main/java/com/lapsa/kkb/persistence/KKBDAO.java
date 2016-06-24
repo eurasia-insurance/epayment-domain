6d098fa20ea08a7152d7c2b5503ca72d78f5ebec
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-24 15:03:14 +0600

    API changed

--- persistence/src/main/java/com/lapsa/kkb/persistence/KKBDAO.java
+++ persistence/src/main/java/com/lapsa/kkb/persistence/KKBDAO.java
@@ -3,5 +3,7 @@ package com.lapsa.kkb.persistence;
 public interface KKBDAO<T, I> {
     T findById(I id) throws KKBEntityNotFound, KKBPeristenceOperationFailed;
 
+    T findByIdForced(final I id) throws KKBEntityNotFound, KKBPeristenceOperationFailed;
+
     <Z extends T> Z save(Z entity) throws KKBPeristenceOperationFailed;
 }
