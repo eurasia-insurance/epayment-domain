2a68b6568e941de1e78f262bbf9aabdafe99609f
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-04 12:16:03 +0600

    KKBOrderDAO.findAllAuthorizated method

--- persistence/src/main/java/com/lapsa/kkb/persistence/KKBOrderDAO.java
+++ persistence/src/main/java/com/lapsa/kkb/persistence/KKBOrderDAO.java
@@ -1,6 +1,9 @@
 package com.lapsa.kkb.persistence;
 
+import java.util.List;
+
 import com.lapsa.kkb.core.KKBOrder;
 
 public interface KKBOrderDAO extends KKBDAO<KKBOrder, String> {
+    List<KKBOrder> findAllAuthorizated();
 }
