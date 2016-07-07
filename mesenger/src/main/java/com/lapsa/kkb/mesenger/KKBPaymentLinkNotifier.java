bbb3ba81cbbc9a77ae64eda121c38fdc2e4f609d
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-07 18:55:38 +0600

    kkb mesenger module

---
+++ mesenger/src/main/java/com/lapsa/kkb/mesenger/KKBPaymentLinkNotifier.java
@@ -0,0 +1,6 @@
+package com.lapsa.kkb.mesenger;
+
+import com.lapsa.kkb.core.KKBPaymentLinkNotification;
+
+public interface KKBPaymentLinkNotifier extends KKBBaseNotifier<KKBPaymentLinkNotification> {
+}
---
+++ mesenger/src/main/java/com/lapsa/kkb/mesenger/KKBPaymentSuccessNotifier.java
@@ -0,0 +1,6 @@
+package com.lapsa.kkb.mesenger;
+
+import com.lapsa.kkb.core.KKBPaymentSuccessNotification;
+
+public interface KKBPaymentSuccessNotifier extends KKBBaseNotifier<KKBPaymentSuccessNotification> {
+}
