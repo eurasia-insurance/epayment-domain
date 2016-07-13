97af96a44fdbb9b16fbb76f74c13e22868aebcd4
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-13 13:57:06 +0600

    new notification DAO methods findByKKBOrder

--- persistence/src/main/java/com/lapsa/kkb/persistence/KKBPaymentLinkNotificationDAO.java
+++ persistence/src/main/java/com/lapsa/kkb/persistence/KKBPaymentLinkNotificationDAO.java
@@ -1,6 +1,10 @@
 package com.lapsa.kkb.persistence;
 
+import java.util.List;
+
+import com.lapsa.kkb.core.KKBOrder;
 import com.lapsa.kkb.core.KKBPaymentLinkNotification;
 
 public interface KKBPaymentLinkNotificationDAO extends BaseKKBNotificationDAO<KKBPaymentLinkNotification> {
+    List<KKBPaymentLinkNotification> findByKKBOrder(KKBOrder order);
 }
--- persistence/src/main/java/com/lapsa/kkb/persistence/KKBPaymentSuccessNotificationDAO.java
+++ persistence/src/main/java/com/lapsa/kkb/persistence/KKBPaymentSuccessNotificationDAO.java
@@ -1,6 +1,10 @@
 package com.lapsa.kkb.persistence;
 
+import java.util.List;
+
+import com.lapsa.kkb.core.KKBOrder;
 import com.lapsa.kkb.core.KKBPaymentSuccessNotification;
 
 public interface KKBPaymentSuccessNotificationDAO extends BaseKKBNotificationDAO<KKBPaymentSuccessNotification> {
+    List<KKBPaymentSuccessNotification> findByKKBOrder(KKBOrder order);
 }
