49491ab55954cdd626755389ce7ed9753b7fee3c
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-27 15:44:33 +0600

    general document dao

---
+++ persistence/src/main/java/com/lapsa/kkb/persistence/KKBGeneralDocumentDAO.java
@@ -0,0 +1,10 @@
+package com.lapsa.kkb.persistence;
+
+import java.util.List;
+
+import com.lapsa.kkb.core.KKBDocument;
+import com.lapsa.kkb.core.KKBOrder;
+
+public interface KKBGeneralDocumentDAO<T extends KKBDocument> extends KKBGeneralDAO<T, Integer> {
+    List<T> findByOrder(KKBOrder order) throws KKBPeristenceOperationFailed;
+}
