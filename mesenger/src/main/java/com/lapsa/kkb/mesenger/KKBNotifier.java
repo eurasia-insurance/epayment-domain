2b6552d80ceb751b0acd784238de44d7f6b03043
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-05-05 14:47:39 +0600

    method rename

--- mesenger/src/main/java/com/lapsa/kkb/mesenger/KKBNotifier.java
+++ mesenger/src/main/java/com/lapsa/kkb/mesenger/KKBNotifier.java
@@ -3,7 +3,7 @@ package com.lapsa.kkb.mesenger;
 import com.lapsa.kkb.core.KKBOrder;
 
 public interface KKBNotifier {
-    void assignRequestNotification(KKBNotificationChannel channel, KKBNotificationRecipientType recipientType,
+    void assignOrderNotification(KKBNotificationChannel channel, KKBNotificationRecipientType recipientType,
 	    KKBNotificationRequestStage stage,
 	    KKBOrder order) throws KKBNotificationFailed;
 }
