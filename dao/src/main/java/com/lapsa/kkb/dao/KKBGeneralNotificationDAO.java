4b36cf43adfef5771cde1b47ac53e904babb52eb
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-05-05 14:29:44 +0600

    notifications dao removed

--- dao/src/main/java/com/lapsa/kkb/dao/KKBGeneralNotificationDAO.java
+++
@@ -1,9 +0,0 @@
-package com.lapsa.kkb.dao;
-
-import java.util.List;
-
-import com.lapsa.kkb.core.KKBNotification;
-
-public interface KKBGeneralNotificationDAO<T extends KKBNotification> extends KKBGeneralDAO<T, Integer> {
-    List<T> findAllPending() throws KKBPeristenceOperationFailed;
-}
