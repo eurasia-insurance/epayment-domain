78db2c62289b9591861d03f08ad17b5255607993
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-09-29 15:50:43 +0600

    aiditional methods and unsable methods

--- services/src/main/java/com/lapsa/kkb/services/KKBFactory.java
+++ services/src/main/java/com/lapsa/kkb/services/KKBFactory.java
@@ -5,14 +5,11 @@ import java.net.URL;
 import com.lapsa.fin.FinCurrency;
 import com.lapsa.kkb.core.KKBOrder;
 import com.lapsa.kkb.core.KKBOrderItem;
-import com.lapsa.kkb.core.KKBPaymentResponseDocument;
 
 public interface KKBFactory {
     String generateNewOrderId();
 
     URL generatePaymentPageUrl(String orderId);
-
-    KKBPaymentResponseDocument buildResponseDocument(String response) throws KKBFormatException;
 
     KKBOrder generateNewOrder(FinCurrency currency, double cost, String product);
 
