9f7bc7d15d6f810f871b57364cb5bc0d8dc957b0
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-19 15:52:31 +0600

    dao moved to epayment-dao

--- dao/src/main/java/com/lapsa/kkb/dao/KKBDocumentDAO.java
+++
@@ -1,6 +0,0 @@
-package com.lapsa.kkb.dao;
-
-import com.lapsa.kkb.core.KKBDocument;
-
-public interface KKBDocumentDAO extends KKBGeneralDocumentDAO<KKBDocument> {
-}
--- dao/src/main/java/com/lapsa/kkb/dao/KKBOrderDAO.java
+++
@@ -1,6 +0,0 @@
-package com.lapsa.kkb.dao;
-
-import com.lapsa.kkb.core.KKBOrder;
-
-public interface KKBOrderDAO extends KKBGeneralDAO<KKBOrder, String> {
-}
