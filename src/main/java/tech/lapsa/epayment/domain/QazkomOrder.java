84e48ae2f1b022d324a24fe91c24415c67822742
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-13 10:14:49 +0600

    QazkomOrder number checked generation

--- src/main/java/tech/lapsa/epayment/domain/QazkomOrder.java
+++ src/main/java/tech/lapsa/epayment/domain/QazkomOrder.java
@@ -6,6 +6,7 @@ import java.time.Instant;
 import java.util.Locale;
 import java.util.StringJoiner;
 import java.util.UUID;
+import java.util.function.Predicate;
 
 import com.lapsa.fin.FinCurrency;
 
@@ -23,13 +24,25 @@ public class QazkomOrder extends AEntity {
     private static final long serialVersionUID = 1L;
     private static final int PRIME = 11;
 
-    public static Long orderNumberAsLong() {
-	final UUID uuid = UUID.randomUUID();
-	return Math.abs(uuid.getLeastSignificantBits() / 10000);
+    public static String generateNumber() {
+	return String.valueOf(Math.abs(UUID.randomUUID().getLeastSignificantBits() / 10000));
     }
 
-    public static String orderNumberAsString() {
-	return String.valueOf(orderNumberAsLong());
+    public static String generateNumber(final Predicate<String> numberIsUnique)
+	    throws NumberOfAttemptsExceedException {
+	MyObjects.requireNonNull(numberIsUnique, "numberIsUnique");
+	final int NUMBER_OF_ATTEMPTS = 100;
+	int attempt = 0;
+	String number;
+	do {
+	    number = generateNumber();
+	    if (attempt++ > NUMBER_OF_ATTEMPTS)
+		throw new NumberOfAttemptsExceedException(
+			String.format(
+				"The number of attempts is exceed the limit (%1$d) while generating the unique number",
+				NUMBER_OF_ATTEMPTS));
+	} while (!numberIsUnique.test(number));
+	return number;
     }
 
     public static QazkomOrderBuilder builder() {
@@ -44,6 +57,7 @@ public class QazkomOrder extends AEntity {
 	private String merchantName;
 	private PrivateKey merchantKey;
 	private X509Certificate merchantCertificate;
+	private Predicate<String> numberIsUnique;
 
 	private QazkomOrderBuilder() {
 	}
@@ -53,16 +67,24 @@ public class QazkomOrder extends AEntity {
 	    return this;
 	}
 
-	public QazkomOrderBuilder withOrderNumber(final String orderNumber) {
+	public QazkomOrderBuilder withNumber(final String orderNumber) {
 	    this.orderNumber = MyStrings.requireNonEmpty(orderNumber, "orderNumber");
+	    this.numberIsUnique = null;
 	    return this;
 	}
 
-	public QazkomOrderBuilder withGeneratedOrderNumber() {
+	public QazkomOrderBuilder withNumber(final String orderNumber, final Predicate<String> numberIsUnique) {
+	    this.orderNumber = MyStrings.requireNonEmpty(orderNumber, "orderNumber");
+	    this.numberIsUnique = MyObjects.requireNonNull(numberIsUnique, "numberIsUnique");
+	    return this;
+	}
+
+	public QazkomOrderBuilder withGeneratedNumber(final Predicate<String> numberIsUnique) {
 	    this.orderNumber = null;
+	    this.numberIsUnique = MyObjects.requireNonNull(numberIsUnique, "numberIsUnique");
 	    return this;
 	}
-
+	
 	public QazkomOrderBuilder withMerchant(final String merchantId, final String merchantName,
 		final X509Certificate merchantCertificate,
 		final PrivateKey merchantKey) {
@@ -76,8 +98,17 @@ public class QazkomOrder extends AEntity {
 	public QazkomOrder build() {
 	    final QazkomOrder result = new QazkomOrder();
 
-	    result.orderNumber = MyOptionals.of(orderNumber) //
-		    .orElseGet(QazkomOrder::orderNumberAsString);
+	    if (MyStrings.empty(orderNumber)) {
+		// using generated value
+		result.orderNumber = MyObjects.nonNull(numberIsUnique) //
+			? Invoice.generateNumber(numberIsUnique) //
+			: Invoice.generateNumber();
+	    } else {
+		// using user value
+		if (MyObjects.nonNull(numberIsUnique) && !numberIsUnique.test(orderNumber))
+		    throw new NonUniqueNumberException(String.format("The number is non-unique (%1$s)", orderNumber));
+		result.orderNumber = orderNumber;
+	    }
 
 	    result.forInvoice = MyObjects.requireNonNull(forInvoice, "forInvoice");
 	    result.amount = MyNumbers.requirePositive(forInvoice.getAmount(), "forInvoice.getAmount");
