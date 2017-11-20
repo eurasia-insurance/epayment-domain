89ce9dca864a1e682b8287d5e55a2acf617695b7
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-20 19:01:50 +0600

    UnknownPaymentBuilder

--- src/main/java/tech/lapsa/epayment/domain/UnknownPayment.java
+++ src/main/java/tech/lapsa/epayment/domain/UnknownPayment.java
@@ -1,9 +1,12 @@
 package tech.lapsa.epayment.domain;
 
 import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.Locale;
 import java.util.StringJoiner;
 
+import tech.lapsa.java.commons.function.MyNumbers;
+import tech.lapsa.java.commons.function.MyObjects;
 import tech.lapsa.java.commons.function.MyOptionals;
 import tech.lapsa.java.commons.localization.Localizeds;
 
@@ -11,6 +14,42 @@ public class UnknownPayment extends Payment {
 
     private static final long serialVersionUID = 1L;
     private static final int PRIME = 23;
+
+    public static UnknownPaymentBuilder builder() {
+	return new UnknownPaymentBuilder();
+    }
+
+    public static final class UnknownPaymentBuilder {
+	private Currency currency;
+	private Double amount;
+
+	private UnknownPaymentBuilder() {
+	}
+
+	public UnknownPaymentBuilder withAmount(final Double amount) {
+	    this.amount = MyNumbers.requirePositive(amount, "amount");
+	    return this;
+	}
+
+	public UnknownPaymentBuilder withCurrency(final Currency currency) {
+	    this.currency = MyObjects.requireNonNull(currency, "currency");
+	    return this;
+	}
+
+	public UnknownPaymentBuilder withAmountAndCurrency(final Double amount, final Currency currency) {
+	    withAmount(amount);
+	    withCurrency(currency);
+	    return this;
+	}
+
+	public UnknownPayment build() {
+	    final UnknownPayment result = new UnknownPayment();
+	    result.currency = MyObjects.requireNonNull(currency, "currency");
+	    result.amount = MyNumbers.requirePositive(amount, "amount");
+
+	    return result;
+	}
+    }
 
     @Override
     protected int prime() {
