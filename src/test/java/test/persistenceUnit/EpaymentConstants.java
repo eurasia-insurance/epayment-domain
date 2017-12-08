382d0175f493e9236fa74f89427482ed812375dd
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-08 18:55:23 +0600

    from jpa-unit

---
+++ src/test/java/test/persistenceUnit/EpaymentConstants.java
@@ -0,0 +1,9 @@
+package test.persistenceUnit;
+
+public final class EpaymentConstants {
+
+    private EpaymentConstants() {
+    }
+
+    public static final String PERSISTENCE_UNIT_NAME = "epaymentPU";
+}
