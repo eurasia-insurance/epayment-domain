1b0365fc40ac851ae2dc60a6dc899cadb234554b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-08 19:27:52 +0600

    jpaunit

---
+++ src/main/java/tech/lapsa/epayment/jpa/EpaymentConstants.java
@@ -0,0 +1,9 @@
+package tech.lapsa.epayment.jpa;
+
+public final class EpaymentConstants {
+
+    private EpaymentConstants() {
+    }
+
+    public static final String PERSISTENCE_UNIT_NAME = "epaymentPU";
+}
