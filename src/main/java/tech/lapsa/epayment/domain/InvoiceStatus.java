4bad0555b0741feeb162b6393cf60a1faa93be07
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-08 17:26:40 +0600

    not used

--- src/main/java/tech/lapsa/epayment/domain/InvoiceStatus.java
+++
@@ -1,7 +0,0 @@
-package tech.lapsa.epayment.domain;
-
-import tech.lapsa.java.commons.localization.LocalizedElement;
-
-public enum InvoiceStatus implements LocalizedElement {
-    PENDING, PAID, EXPIRED;
-}
