69fa404f93c0b9a9253df8087490d8c35f9730bd
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-11 16:54:23 +0600

    on-fly calculation

--- src/main/java/tech/lapsa/epayment/domain/Domain.java
+++ src/main/java/tech/lapsa/epayment/domain/Domain.java
@@ -1,7 +1,6 @@
 package tech.lapsa.epayment.domain;
 
 import java.io.Serializable;
-import java.util.Locale;
 
 import javax.persistence.MappedSuperclass;
 
@@ -13,24 +12,17 @@ public abstract class Domain implements Localized, Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private final Integer prime;
-    private final Integer multiplier;
-    private final Locale locale;
-
     public Domain() {
-	prime = MyHcEqToStr.primeOf(this.getClass());
-	multiplier = MyHcEqToStr.multiplierOf(this.getClass(), prime);
-	locale = MyHcEqToStr.toStringLocaleOf(this.getClass());
     }
 
     @Override
     public String toString() {
-	return localized(LocalizationVariant.NORMAL, locale);
+	return localized(LocalizationVariant.NORMAL, MyHcEqToStr.toStringLocaleOf(this.getClass()));
     }
 
     @Override
     public final int hashCode() {
-	return MyHcEqToStr.hashCode(this, prime, multiplier);
+	return MyHcEqToStr.hashCode(this, MyHcEqToStr.primeOf(this.getClass()));
     }
 
     @Override
