9ea0cc2765d3cb314744e8673ad1da478879c4bf
Vadim.Isaev <vadim.o.isaev@gmail.com>
2019-01-27 02:05:52 +0300

    new invoice status

--- src/main/java/tech/lapsa/epayment/domain/InvoiceStatus.java
+++ src/main/java/tech/lapsa/epayment/domain/InvoiceStatus.java
@@ -3,5 +3,5 @@ package tech.lapsa.epayment.domain;
 import tech.lapsa.java.commons.localization.LocalizedElement;
 
 public enum InvoiceStatus implements LocalizedElement {
-    PENDING, PAID, EXPIRED;
+    PENDING, PAID, EXPIRED, PAYMENT_CANCELED;
 }
